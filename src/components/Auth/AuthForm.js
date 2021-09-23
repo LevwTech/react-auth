90040725bf384ad38fc1f8a740bed0416bc257ec
levvw <abdelraahmanmostafa@gmail.com>
2021-09-23 21:41:33 +0200

    fetched email and password to firebase authentication

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,34 +1,75 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import classes from './AuthForm.module.css';
+import classes from "./AuthForm.module.css";
 
 const AuthForm = () => {
   const [isLogin, setIsLogin] = useState(true);
+  const [userEmail, setEmail] = useState("");
+  const [userPassword, setPassword] = useState("");
+
+  function onChangeEmailHandler(e) {
+    setEmail(e.target.value);
+  }
+  function onChangePasswordHandler(e) {
+    setPassword(e.target.value);
+  }
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
 
+  function submitHandler(e) {
+    e.preventDefault();
+    if (isLogin) {
+    } else {
+      fetch(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBuseaVZfJgxqYPMwsGInpPAov6bgkx-As",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            email: userEmail,
+            password: userPassword,
+            returnSecureToken: true,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      )
+        .then((res) => res.json)
+        .then((data) => console.log(data));
+    }
+  }
   return (
     <section className={classes.auth}>
-      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
+      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
-          <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <label htmlFor="email">Your Email</label>
+          <input
+            type="email"
+            id="email"
+            required
+            onChange={onChangeEmailHandler}
+          />
         </div>
         <div className={classes.control}>
-          <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <label htmlFor="password">Your Password</label>
+          <input
+            type="password"
+            id="password"
+            required
+            onChange={onChangePasswordHandler}
+          />
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
+          <button>{isLogin ? "Login" : "Create Account"}</button>
           <button
-            type='button'
+            type="button"
             className={classes.toggle}
             onClick={switchAuthModeHandler}
           >
-            {isLogin ? 'Create new account' : 'Login with existing account'}
+            {isLogin ? "Create new account" : "Login with existing account"}
           </button>
         </div>
       </form>
