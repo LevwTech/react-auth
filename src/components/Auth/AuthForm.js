9c5f5606823e6a74454e649e33debc6ac78da4ab
levvw <abdelraahmanmostafa@gmail.com>
2021-09-24 19:50:56 +0200

    redirecting the user after login and after changing password

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,15 +1,19 @@
 import { useState } from "react";
-import { useSelector, useDispatch } from "react-redux";
+import React from "react";
+import { useDispatch } from "react-redux";
+// import { useSelector } from "react-redux";
 import classes from "./AuthForm.module.css";
 import types from "../../store/actionTypes";
+import { Redirect } from "react-router-dom";
 
 const AuthForm = () => {
+  const [goToHome, setGoToHome] = useState(false);
   const dispatch = useDispatch();
   const [isLogin, setIsLogin] = useState(true);
   const [userEmail, setEmail] = useState("");
   const [userPassword, setPassword] = useState("");
-  const token = useSelector((state) => state.token);
-  const isLoggedIn = useSelector((state) => state.isLoggedIn);
+  // const token = useSelector((state) => state.token);
+  // const isLoggedIn = useSelector((state) => state.isLoggedIn);
 
   function onChangeEmailHandler(e) {
     setEmail(e.target.value);
@@ -46,47 +50,50 @@ const AuthForm = () => {
       .then((res) => res.json())
       .then((data) => {
         if (data.error) alert(data.error.message);
-        console.log(data);
-        dispatch({ type: types.LOGIN, payload: data.idToken });
-        console.log(token);
-        console.log(isLoggedIn);
+        else {
+          dispatch({ type: types.LOGIN, payload: data.idToken });
+          setGoToHome(true);
+        }
       })
       .catch((e) => alert(e.message));
   }
   return (
-    <section className={classes.auth}>
-      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
-      <form onSubmit={submitHandler}>
-        <div className={classes.control}>
-          <label htmlFor="email">Your Email</label>
-          <input
-            type="email"
-            id="email"
-            required
-            onChange={onChangeEmailHandler}
-          />
-        </div>
-        <div className={classes.control}>
-          <label htmlFor="password">Your Password</label>
-          <input
-            type="password"
-            id="password"
-            required
-            onChange={onChangePasswordHandler}
-          />
-        </div>
-        <div className={classes.actions}>
-          <button>{isLogin ? "Login" : "Create Account"}</button>
-          <button
-            type="button"
-            className={classes.toggle}
-            onClick={switchAuthModeHandler}
-          >
-            {isLogin ? "Create new account" : "Login with existing account"}
-          </button>
-        </div>
-      </form>
-    </section>
+    <React.Fragment>
+      {goToHome && <Redirect to="/"></Redirect>}
+      <section className={classes.auth}>
+        <h1>{isLogin ? "Login" : "Sign Up"}</h1>
+        <form onSubmit={submitHandler}>
+          <div className={classes.control}>
+            <label htmlFor="email">Your Email</label>
+            <input
+              type="email"
+              id="email"
+              required
+              onChange={onChangeEmailHandler}
+            />
+          </div>
+          <div className={classes.control}>
+            <label htmlFor="password">Your Password</label>
+            <input
+              type="password"
+              id="password"
+              required
+              onChange={onChangePasswordHandler}
+            />
+          </div>
+          <div className={classes.actions}>
+            <button>{isLogin ? "Login" : "Create Account"}</button>
+            <button
+              type="button"
+              className={classes.toggle}
+              onClick={switchAuthModeHandler}
+            >
+              {isLogin ? "Create new account" : "Login with existing account"}
+            </button>
+          </div>
+        </form>
+      </section>
+    </React.Fragment>
   );
 };
 
--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,17 +1,56 @@
-import classes from './ProfileForm.module.css';
+import classes from "./ProfileForm.module.css";
+import { useState } from "react";
+import { Redirect } from "react-router-dom";
+import { useSelector } from "react-redux";
+import React from "react";
 
 const ProfileForm = () => {
+  const [newPassword, setNewPassword] = useState("");
+  const userToken = useSelector((state) => state.token);
+  const [goToHome, setGoToHome] = useState(false);
+
+  function onChangeHandler(e) {
+    setNewPassword(e.target.value);
+  }
+  function onClickHandler(e) {
+    e.preventDefault();
+    fetch(
+      "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyBuseaVZfJgxqYPMwsGInpPAov6bgkx-As",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          idToken: userToken,
+          password: newPassword,
+          returnSecureToken: false,
+        }),
+      }
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.error) alert(data.error.message);
+        else {
+          setGoToHome(true);
+        }
+      })
+      .catch((e) => alert(e));
+  }
   return (
-    <form className={classes.form}>
-      <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
-      </div>
-      <div className={classes.action}>
-        <button>Change Password</button>
-      </div>
-    </form>
+    <React.Fragment>
+      {goToHome && <Redirect to="/"></Redirect>}
+      <form className={classes.form}>
+        <div className={classes.control}>
+          <label htmlFor="new-password">New Password</label>
+          <input onChange={onChangeHandler} type="password" id="new-password" />
+        </div>
+        <div className={classes.action}>
+          <button onClick={onClickHandler}>Change Password</button>
+        </div>
+      </form>
+    </React.Fragment>
   );
-}
+};
 
 export default ProfileForm;
