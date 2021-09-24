3f800553cdf34d5fef12d38ba46694a007d7bf86
levvw <abdelraahmanmostafa@gmail.com>
2021-09-24 21:01:38 +0200

    added protected pages (bool isLoggedIn state from redux on routes)

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import { Switch, Route } from "react-router-dom";
-
+import { Switch, Route, Redirect } from "react-router-dom";
+import { useSelector } from "react-redux";
 import Layout from "./components/Layout/Layout";
 import UserProfile from "./components/Profile/UserProfile";
 import AuthPage from "./pages/AuthPage";
 import HomePage from "./pages/HomePage";
 
 function App() {
+  const isLoggedIn = useSelector((state) => state.isLoggedIn);
   return (
     <Layout>
       <Switch>
         <Route path="/" exact>
           <HomePage />
         </Route>
-        <Route path="/auth">
-          <AuthPage />
-        </Route>
-        <Route path="/profile">
-          <UserProfile />
+        {!isLoggedIn && (
+          <Route path="/auth">
+            <AuthPage />
+          </Route>
+        )}
+        {isLoggedIn && (
+          <Route path="/profile">
+            <UserProfile />
+          </Route>
+        )}
+        <Route path="*">
+          <Redirect to="/" />
         </Route>
       </Switch>
     </Layout>
